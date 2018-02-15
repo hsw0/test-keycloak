bfc463629b0ffa943a34356c11355c7231eadbe9
hsw <hsw@syscall.io>
2018-02-15 21:47:45 +0900

    provision: dnsmasq 설정 Ansible로 재작성

--- provision/keycloak/provision.sh
+++ provision/keycloak/provision.sh
@@ -13,11 +13,6 @@ cp -f /vagrant/provision/nginx/conf.d/keycloak.conf /etc/nginx/conf.d/
 
 systemctl restart nginx.service && systemctl enable nginx.service
 
-# Samba DC를 DNS resolver로 직접 지정시 DC가 죽은 경우
-# 패키지 설치, 외부 IDP 로그인시 외부 DNS 조회 실패를 방지하기 위해
-# dnsmasq를 Split-horizon DNS 용으로 사용
-/vagrant/provision/scripts/dnsmasq.sh "$(hostname --domain)" 192.168.33.224
-
 # Install & Provision keycloak
 /vagrant/provision/keycloak/setup.sh
 
